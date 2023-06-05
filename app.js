77f0a2a2a6cd768566da7244f34a9bdc2765a79a
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2023-06-05 14:44:07 +0300

    remove logging in file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const moment = require('moment');
 require('dotenv').config();
 
-const fs = require('fs/promises');
+// const moment = require('moment');
+// const fs = require('fs/promises');
 
 const productsRouter = require('./routes/api/products');
 
@@ -13,12 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(async (request, __, next) => {
-  const { method, url } = request;
-  const date = moment().format('DD-MM-YYYY_hh:mm:ss');
-  await fs.appendFile('./public/server.log', `\n${method} ${url} ${date}`);
-  next();
-});
+// app.use(async (request, __, next) => {
+//   const { method, url } = request;
+//   const date = moment().format('DD-MM-YYYY_hh:mm:ss');
+//   await fs.appendFile('./public/server.log', `\n${method} ${url} ${date}`);
+//   next();
+// });
 
 app.use('/api/products', productsRouter);
 
