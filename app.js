4c7d2025e19b47e9a08105d9f7f5d1796c7bcf13
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2023-06-06 19:06:15 +0300

    logging is off

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const moment = require('moment');
-const fs = require('fs/promises');
+// const moment = require('moment');
+// const fs = require('fs/promises');
 
 const productsRouter = require('./routes/api/products');
 const ordersRouter = require('./routes/api/orders');
@@ -14,12 +14,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(async (request, __, next) => {
-  const { method, url } = request;
-  const date = moment().format('DD-MM-YYYY_hh:mm:ss');
-  await fs.appendFile('./public/server.log', `\n${method} ${url} ${date}`);
-  next();
-});
+// app.use(async (request, __, next) => {
+//   const { method, url } = request;
+//   const date = moment().format('DD-MM-YYYY_hh:mm:ss');
+//   await fs.appendFile('./public/server.log', `\n${method} ${url} ${date}`);
+//   next();
+// });
 
 app.use('/api/products', productsRouter);
 app.use('/api/orders', ordersRouter);
